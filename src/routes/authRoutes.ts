29ee1697536101b29704e84ad1aa41c9a886523e
Hanafiantho <hanafiantho@gmail.com>
2025-10-15 16:06:09 +0700

    add change password validator

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { changePassword, login } from '../controllers/authController';
 import { validate } from '../middleware/validate';
-import { loginValidator } from '../validators/authValidator';
+import { changePasswordValidator, loginValidator } from '../validators/authValidator';
 
 const router = Router();
 
 router.post('/login', loginValidator, validate, login);
-router.put('/change-password/:id', changePassword);
+router.put('/change-password/:id', changePasswordValidator, validate, changePassword);
 
 export default router;
--- src/validators/authValidator.ts
+++ src/validators/authValidator.ts
@@ -15,3 +15,21 @@ export const loginValidator = [
         .matches(/[A-Z]/)
         .withMessage('password must be contain at least one uppercase letter'),
 ];
+
+export const changePasswordValidator = [
+    body('oldPassword')
+        .notEmpty()
+        .withMessage('old password cannot be empty')
+        .isLength({ min: 6 })
+        .withMessage('old password must be at least 6 characters')
+        .matches(/[A-Z]/)
+        .withMessage('old password must be contain at least one uppercase letter'),
+
+    body('newPassword')
+        .notEmpty()
+        .withMessage('new password cannot be empty')
+        .isLength({ min: 6 })
+        .withMessage('new password must be at least 6 characters')
+        .matches(/[A-Z]/)
+        .withMessage('new password must be contain at least one uppercase letter'),
+];
